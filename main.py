d54c55781145e7ae137e36f41ea3c95b2acab81a
EJune <ejunef09@gmail.com>
2023-02-02 18:14:09 +0800

    design code structure as comments

---
+++ main.py
@@ -0,0 +1,22 @@
+#class comb
+#class include
+#def next not repeat combination
+#print title
+
+#settings input number of digits
+
+
+#do while loop
+while True :
+    #guess num
+	#if guess num not possible continue
+
+    #print guess num
+    #get A
+    #get B
+    #if A = full break
+    #if nA0B (generate comb)
+    
+    #include N numbers only (delete comb and add )
+	pass
+
